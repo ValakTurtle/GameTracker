e37d92d8dbdb462dc61d73043f344c09f5e3c85d
Collin Hemze <71791303+ValakTurtle@users.noreply.github.com>
2025-06-28 12:22:29 -0500

    Wait, forgot app.run

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ if __name__ == "__main__":
         webbrowser.open_new("http://127.0.0.1:5000/")
 
     Timer(1, open_browser).start()
-    #app.run(debug=True, use_reloader=False)  # Disable reloader to prevent multiple instances of the browser from opening
+    app.run(debug=True, use_reloader=False)  # Disable reloader to prevent multiple instances of the browser from opening
