7489ed259a35a3b6f5ca56b640a04f0f28cf00d1
Collin Hemze <71791303+ValakTurtle@users.noreply.github.com>
2025-06-29 00:33:00 -0500

    Added Steam as platform option and added a to-do list

--- app.py
+++ app.py
@@ -198,3 +198,11 @@ if __name__ == "__main__":
     if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
         Timer(1, open_browser).start()
     app.run(debug=True)
+
+
+
+
+# Make the Cover Image URL work
+# Remove game rating?
+# Have the games be sorted in alphabetical order
+# Remove Start Date and Finish Date sections in Add Game page, will have that stuff be automatic
